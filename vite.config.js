904c0e59d19148c66917fa8f2f398e31fc90a0b9
Tharun546 <tharun.p147@gmail.com>
2024-10-23 11:22:16 -0500

    fix bugs

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/",
   css: {
     postcss: {
       plugins: [tailwindcss()],
