16b271981346af1434dd985f151383a1dd5c3fbc
Tharun546 <tharun.p147@gmail.com>
2025-04-05 01:00:43 -0500

    copde update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -159,7 +159,7 @@ export default function Projects() {
         <a
           className="inline-flex items-center font-medium leading-tight text-slate-200 font-semibold text-slate-200 group"
           aria-label="View Full Project Archive"
-          href="/archive/projects"
+          href="/portfolio/archive/projects"
         >
           <span>
             <span className="border-b border-transparent pb-px transition group-hover:border-teal-300 motion-reduce:transition-none">
